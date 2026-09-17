1ba310061453ad048d87a353c82fe11bc8d6c96e
julialuciani14 <lucianistefanojulia@gmail.com>
2026-09-17 20:40:32 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function adivinheNumero(){
-    alert("Pensei em um número entre 1 a 100. Tente adivinha o número!")
-    const num1 = Math.floor(Math.random() * 100) + 1
-    let resposta = prompt("Palpite")
-    let tentativa = 0
+alert("Pensei em um número entre 1 a 100. Tente adivinha o número!")
+const num1 = Math.floor(Math.random() * 100) + 1
+let resposta = prompt("Palpite")
+let tentativa = 0
 
-    while(resposta  != num1){
-        tentativa++
+while(resposta  != num1){
+    tentativa++
 
-    if(num1 > resposta ){
-     alert ("Muito Baixo!!")
+if(num1 > resposta ){
+    alert ("Muito Baixo!!")
 
+}else{
+    alert ("Muito Alto!!")
 
-    }else{
-        alert ("Muito Alto!!")
-
-    }
-        resposta = prompt ("Palpite: ")
-        tentativa++
-    }
-         alert("Vocé acertou " + tentativa + "Tentativas")
-
-    }
+}
+    resposta = prompt ("Palpite: ")
+    tentativa++
+}
+    alert("Vocé acertou " + tentativa + "Tentativas")
+   
+}
         
